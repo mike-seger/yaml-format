1420a38ba1190f30f0340fe49981418ca0992812
michael <gitlab@net128.com>
2021-07-04 21:33:18 +0200

    cleanup 1

--- lib/src/main/java/com/net128/util/yaml/format/YamlFormatter.java
+++ lib/src/main/java/com/net128/util/yaml/format/YamlFormatter.java
@@ -1,8 +1,5 @@
 package com.net128.util.yaml.format;
 
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,134 +7,144 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
 @SuppressWarnings("unchecked")
 public class YamlFormatter {
-    public String format(String multiInput) {
-        var docSeparator = "---\n";
-        return Arrays.stream(multiInput.split("\n"+docSeparator))
-            .map(this::formatSingle).filter(Objects::nonNull)
-            .collect(Collectors.joining(docSeparator));
-    }
+	public String format(String multiInput) {
+		var docSeparator = "---\n";
+		return Arrays.stream(multiInput.split("\n" + docSeparator))
+				.map(this::formatSingle)
+				.filter(Objects::nonNull)
+				.collect(Collectors.joining(docSeparator));
+	}
 
-    public String formatSingle(String input) {
-        return formatSingle(input, true);
-    }
+	public String formatSingle(String input) {
+		return formatSingle(input, true);
+	}
 
-    public String formatSingle(String input, boolean mapDotted) {
-        var yaml = new Yaml();
-        Map<String, Object> tree = yaml.loadAs(input, HashMap.class);
-        if(tree == null) {
-            return null;
-        }
-        if(mapDotted) tree = mapDottedKeys(tree);
-        DumperOptions options = new DumperOptions();
-        options.setIndent(2);
-        options.setPrettyFlow(true);
-        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-        var yamlOut = new Yaml(options);
-        return yamlOut.dump(collapseSingletonMaps(tree));
-    }
+	public String formatSingle(String input, boolean mapDotted) {
+		var yaml = new Yaml();
+		Map<String, Object> tree = yaml.loadAs(input, HashMap.class);
+		if (tree == null) {
+			return null;
+		}
+		if (mapDotted) tree = mapDottedKeys(tree);
+		DumperOptions options = new DumperOptions();
+		options.setIndent(2);
+		options.setPrettyFlow(true);
+		options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+		var yamlOut = new Yaml(options);
+		return yamlOut.dump(collapseSingletonMaps(tree));
+	}
 
-    public String identity(String input) {
-        var yaml = new Yaml();
-        Map<String, Object> tree = yaml.loadAs(input, HashMap.class);
-        if(tree == null) {
-            return null;
-        }
-        DumperOptions options = new DumperOptions();
-        options.setIndent(2);
-        options.setPrettyFlow(true);
-        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-        var yamlOut = new Yaml(options);
-        return yamlOut.dump(tree);
-    }
+	public String identity(String input) {
+		var yaml = new Yaml();
+		Map<String, Object> tree = yaml.loadAs(input, HashMap.class);
+		if (tree == null) {
+			return null;
+		}
+		DumperOptions options = new DumperOptions();
+		options.setIndent(2);
+		options.setPrettyFlow(true);
+		options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+		var yamlOut = new Yaml(options);
+		return yamlOut.dump(tree);
+	}
 
-    public String format(File file) throws IOException {
-        return format(Files.readString(file.toPath()));
-    }
+	public String format(File file) throws IOException {
+		return format(Files.readString(file.toPath()));
+	}
 
-    private Map<String, Object> collapseSingletonMaps(final Map<String, Object> rootTree) {
-        var result=new TreeMap<String, Object>();
-        var aliasValueMap= new HashMap<>();
-        rootTree.forEach((key, value) -> addCollapsedSubTree(result, new Node(key, value), aliasValueMap));
-        return result;
-    }
+	private Map<String, Object> collapseSingletonMaps(final Map<String, Object> rootTree) {
+		var result = new TreeMap<String, Object>();
+		var aliasValueMap = new HashMap<>();
+		rootTree.forEach(
+				(key, value) -> addCollapsedSubTree(result, new Node(key, value), aliasValueMap));
+		return result;
+	}
 
-    private void addCollapsedSubTree(final Map<String, Object> parentTree, final Node node, Map<Object, Object> aliasValueMap) {
-        var newNode = node;
-        if(aliasValueMap.containsKey(node.value)) {
-            newNode = new Node(node.key, aliasValueMap.get(node.value));
-        } else if(node.value instanceof Map) {
-            var childTree = (Map<String, Object>)node.value;
-            if(childTree.size()==1) {
-                addCollapsedSubTree(parentTree, new Node(node.key, (childTree.entrySet().iterator().next())), aliasValueMap);
-                newNode = null; //do not add this node to parent
-            } else {
-                var newChildTree = new TreeMap<String, Object>();
-                childTree.forEach((key, value) -> addCollapsedSubTree(newChildTree, new Node(key, value), aliasValueMap));
-                newNode = new Node(node.key, newChildTree);
-                aliasValueMap.put(node.value, newNode.value);
-            }
-        }
-        addNodeToTree(parentTree, newNode);
-    }
+	private void addCollapsedSubTree(
+			final Map<String, Object> parentTree, final Node node, Map<Object, Object> aliasValueMap) {
+		var newNode = node;
+		if (aliasValueMap.containsKey(node.value)) {
+			newNode = new Node(node.key, aliasValueMap.get(node.value));
+		} else if (node.value instanceof Map) {
+			var childTree = (Map<String, Object>) node.value;
+			if (childTree.size() == 1) {
+				addCollapsedSubTree(
+						parentTree,
+						new Node(node.key, (childTree.entrySet().iterator().next())),
+						aliasValueMap);
+				newNode = null; // do not add this node to parent
+			} else {
+				var newChildTree = new TreeMap<String, Object>();
+				childTree.forEach(
+						(key, value) -> addCollapsedSubTree(newChildTree, new Node(key, value), aliasValueMap));
+				newNode = new Node(node.key, newChildTree);
+				aliasValueMap.put(node.value, newNode.value);
+			}
+		}
+		addNodeToTree(parentTree, newNode);
+	}
 
-    private Map<String, Object> mapDottedKeys(final Map<String, Object> rootTree) {
-        var result=new TreeMap<String, Object>();
-        rootTree.forEach((key, value) -> addMappedSubTreeKeys(result, new Node(key, value)));
-        return result;
-    }
+	private Map<String, Object> mapDottedKeys(final Map<String, Object> rootTree) {
+		var result = new TreeMap<String, Object>();
+		rootTree.forEach((key, value) -> addMappedSubTreeKeys(result, new Node(key, value)));
+		return result;
+	}
 
-    private void addMappedSubTreeKeys(Map<String, Object> parentTree, Node node) {
-        if(node.key.contains(".")) {
-            var keys = node.key.split("[.]");
-            node.key = keys[keys.length-1];
-            for(int i=0; i<keys.length-1; i++) {
-                parentTree = (Map<String, Object>)addNodeToTree(parentTree, keys[i], new HashMap<String, Object>());
-            }
-        }
-        addNodeToTree(parentTree, node);
-    }
+	private void addMappedSubTreeKeys(Map<String, Object> parentTree, Node node) {
+		if (node.key.contains(".")) {
+			var keys = node.key.split("[.]");
+			node.key = keys[keys.length - 1];
+			for (int i = 0; i < keys.length - 1; i++) {
+				parentTree =
+						(Map<String, Object>) addNodeToTree(parentTree, keys[i], new HashMap<String, Object>());
+			}
+		}
+		addNodeToTree(parentTree, node);
+	}
 
-    private void addNodeToTree(Map<String, Object> tree, Node node) {
-        if (node != null) {
-            addNodeToTree(tree, node.key, node.value);
-        }
-    }
+	private void addNodeToTree(Map<String, Object> tree, Node node) {
+		if (node != null) {
+			addNodeToTree(tree, node.key, node.value);
+		}
+	}
 
-    private Object addNodeToTree(Map<String, Object> tree, String key, Object value) {
-        Object existingValue=tree.get(key);
-        if(existingValue==null) {
-            tree.put(key, value);
-            return value;
-        } else if(existingValue == value) {
-            return value;
-        } else if(existingValue instanceof Map && value instanceof Map) {
-            var mergedMaps = mergeMaps((Map<String, Object>) existingValue, (Map<String, Object>) value);
-            tree.put(key, mergedMaps);
-            return mergedMaps;
-        } else {
-            throw new IllegalStateException("Merge collision at key: "+key);
-        }
-    }
+	private Object addNodeToTree(Map<String, Object> tree, String key, Object value) {
+		Object existingValue = tree.get(key);
+		if (existingValue == null) {
+			tree.put(key, value);
+			return value;
+		} else if (existingValue == value) {
+			return value;
+		} else if (existingValue instanceof Map && value instanceof Map) {
+			var mergedMaps = mergeMaps((Map<String, Object>) existingValue, (Map<String, Object>) value);
+			tree.put(key, mergedMaps);
+			return mergedMaps;
+		} else {
+			throw new IllegalStateException("Merge collision at key: " + key);
+		}
+	}
 
-    private Map<String, Object> mergeMaps(Map<String, Object> map1, Map<String, Object> map2) {
-        return Stream.concat(map1.entrySet().stream(),
-            map2.entrySet().stream()).collect(
-            Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
+	private Map<String, Object> mergeMaps(Map<String, Object> map1, Map<String, Object> map2) {
+		return Stream.concat(map1.entrySet().stream(), map2.entrySet().stream())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
 
-    private static class Node {
-        String key;
-        Object value;
+	private static class Node {
+		String key;
+		Object value;
 
-        public Node(String prefix, Map.Entry<String, Object> entry) {
-            this(prefix+"."+entry.getKey(), entry.getValue());
-        }
+		public Node(String prefix, Map.Entry<String, Object> entry) {
+			this(prefix + "." + entry.getKey(), entry.getValue());
+		}
 
-        public Node(String key, Object value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
+		public Node(String key, Object value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
 }
--- lib/src/test/java/com/net128/util/yaml/format/YamlFormatterTest.java
+++ lib/src/test/java/com/net128/util/yaml/format/YamlFormatterTest.java
@@ -1,51 +1,57 @@
 package com.net128.util.yaml.format;
 
-import com.net128.util.yaml.ResourceLoader;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.yaml.snakeyaml.constructor.ConstructorException;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import com.net128.util.yaml.ResourceLoader;
 
 public class YamlFormatterTest implements ResourceLoader {
 	private final YamlFormatter yamlFormatter = new YamlFormatter();
+
 	@ParameterizedTest(name = "{1} -> {0}.")
 	@CsvSource({
-		"application1-formatted.yaml, application1.yaml"
-		,"application1-formatted.yaml, application1-formatted.yaml"
-		,"application2-formatted.yaml, application2.yaml"
-		,"application2-formatted.yaml, application2-formatted.yaml"
-		,"single-node-formatted.yaml, single-node.yaml"
-		,"dotted-keys-formatted.yaml, dotted-keys.yaml"
-		,"un-dotted-keys-formatted.yaml, un-dotted-keys.yaml"
-		,"multi-doc-formatted.yaml, multi-doc.yaml"
-		,"aliases-formatted.yaml, aliases.yaml"
+		"application1-formatted.yaml, application1.yaml",
+		"application1-formatted.yaml, application1-formatted.yaml",
+		"application2-formatted.yaml, application2.yaml",
+		"application2-formatted.yaml, application2-formatted.yaml",
+		"single-node-formatted.yaml, single-node.yaml",
+		"dotted-keys-formatted.yaml, dotted-keys.yaml",
+		"un-dotted-keys-formatted.yaml, un-dotted-keys.yaml",
+		"multi-doc-formatted.yaml, multi-doc.yaml",
+		"aliases-formatted.yaml, aliases.yaml"
 	})
 	@DisplayName("Test formatted YAML against expected result.")
-	public void testFormatYaml(String expected, String actual) throws IOException, URISyntaxException {
+	public void testFormatYaml(String expected, String actual)
+			throws IOException, URISyntaxException {
 		assertEquals(fromResource(expected), yamlFormatter.format(fromResource(actual)));
 	}
 
 	@ParameterizedTest(name = "{1} -> {0}.")
-	@CsvSource({
-			"issue-mod-aliases-formatted.yaml, issue-mod-aliases.yaml"
-		//	,"issue-merge-valid.yaml, issue-merge-valid.yaml"
+	@CsvSource({"issue-mod-aliases-formatted.yaml, issue-mod-aliases.yaml"
+			//	,"issue-merge-valid.yaml, issue-merge-valid.yaml"
 	})
 	@DisplayName("Test identity YAML against expected result.")
-	public void testIdentityYaml(String expected, String actual) throws IOException, URISyntaxException {
+	public void testIdentityYaml(String expected, String actual)
+			throws IOException, URISyntaxException {
 		assertNotEquals(fromResource(expected), yamlFormatter.identity(fromResource(actual)));
 	}
 
 	@Test
 	@DisplayName("Test invalid YAML.")
 	public void testFormatYamlFail() {
-		Exception thrown = assertThrows(IllegalStateException.class, () -> yamlFormatter.format(fromResource("merge1-fail.yaml")));
+		Exception thrown =
+				assertThrows(
+						IllegalStateException.class,
+						() -> yamlFormatter.format(fromResource("merge1-fail.yaml")));
 		assertEquals("Merge collision at key: jkl.mno", thrown.getMessage());
 		assertNull(thrown.getCause());
 	}
@@ -53,7 +59,10 @@ public class YamlFormatterTest implements ResourceLoader {
 	@Test
 	@DisplayName("Test invalid YAML.")
 	public void testFormatYamlFailMerge() {
-		Exception thrown = assertThrows(IllegalStateException.class, () -> yamlFormatter.format(fromResource("merge2-fail.yaml")));
+		Exception thrown =
+				assertThrows(
+						IllegalStateException.class,
+						() -> yamlFormatter.format(fromResource("merge2-fail.yaml")));
 		assertEquals("Merge collision at key: def", thrown.getMessage());
 		assertNull(thrown.getCause());
 	}
@@ -61,9 +70,13 @@ public class YamlFormatterTest implements ResourceLoader {
 	@Test
 	@DisplayName("Test issue with valid YAML.")
 	public void testValidIssue() {
-		Exception thrown = assertThrows(ConstructorException.class, () -> yamlFormatter.format(fromResource("issue-merge-valid.yaml")));
-		assertLinesMatch(List.of("Can't construct a java object for tag:yaml.org,2002:java.util.HashMap.*"),
-			List.of(thrown.getMessage().replaceAll("\n.*", "")));
+		Exception thrown =
+				assertThrows(
+						ConstructorException.class,
+						() -> yamlFormatter.format(fromResource("issue-merge-valid.yaml")));
+		assertLinesMatch(
+				List.of("Can't construct a java object for tag:yaml.org,2002:java.util.HashMap.*"),
+				List.of(thrown.getMessage().replaceAll("\n.*", "")));
 		assertNotNull(thrown.getCause());
 	}
 }
